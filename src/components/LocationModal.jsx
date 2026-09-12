15d64d96223e96a4e65878735f55403aac23eff9
KI-Anik <iamkhairul101@gmail.com>
2026-09-12 15:53:59 +0600

    feat: add city input form and submit functionality to LocationModal component

--- src/components/LocationModal.jsx
+++ src/components/LocationModal.jsx
@@ -1,12 +1,48 @@
 import { CircleX } from "lucide-react";
+import { useState } from "react";
 
-const LocationModal = ({onClose}) => {
+const LocationModal = ({ onClose }) => {
+    const [city, setCity] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const value = city.trim()
+        console.log(value);
+    }
     return (
         <div className="fixed inset-0 flex justify-center items-center bg-gray-950/60">
             <div className="w-100 h-75 p-5 bg-gray-100 shadow-2xl rounded-2xl">
                 <div className="flex justify-between items-center">
                     <h2 className="font-medium text-xl">Where are you from?</h2>
                     <button onClick={onClose} className="cursor-pointer"><CircleX /></button>
+                </div>
+                <div className="pt-5">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="space-y-5">
+                        <input
+                            onChange={(e) => setCity(e.target.value)}
+                            value={city}
+                            placeholder="Enter city name"
+                            type="text"
+                            className="border p-1 w-full rounded-2xl" />
+
+                        {/* button start */}
+                        <div className="">
+                            <button type="submit"
+                                className="w-full text-lg text-gray-100 bg-blue-500 px-5 py-1 font-medium hover:scale-105 transition-all delay-500 rounded-4xl">
+                                Get weather
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+                <div className="text-center text-xl py-1">or</div>
+
+                <div className="">
+                    <button type="button"
+                        className="w-full text-lg text-gray-100 bg-blue-500 px-5 py-1 font-medium hover:scale-105 transition-all delay-500 rounded-4xl">
+                        Use My Locations
+                    </button>
                 </div>
             </div>
         </div>
