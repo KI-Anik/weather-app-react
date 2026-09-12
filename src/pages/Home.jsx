d643771a3f70af87ff787a80a69570161879f075
KI-Anik <iamkhairul101@gmail.com>
2026-09-12 15:00:03 +0600

    feat: enhance Home component with state management and styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 
 const Home = () => {
+    const [click, setClick] = useState(false)
+    console.log(click);
+
     return (
-        <div>
-            <h1 className= "text-6xl text-blue-200">
+        <div className="text-center">
+            <h1 className= "text-6xl text-blue-200 font-extrabold">
                 Titan <span className="text-blue-400">Weather</span>
             </h1>
+            <p className="py-4 text-xl  text-gray-400">Check your Weather today</p>
+            <button onClick={()=> setClick(true)} type="button" className="text-lg text-gray-100 bg-blue-500 px-5 py-1 font-medium hover:scale-105 transition-all delay-500 rounded-4xl">Check weather</button>
         </div>
     );
 };
